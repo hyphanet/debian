2e3725d899336a007e284bd49f3126b9940e40c6
Masayuki Hatta <mhatta@mhatta.org>
2010-08-03 13:39:01 +0900

    Reorganized the directory structure

--- freenet-daemon/00update-fred.sh
+++ freenet-daemon/00update-fred.sh
@@ -14,37 +14,37 @@ cd ../..
 GIT_DESCRIBED=$(cd fred-${FREENET_BRANCH} && git describe && cd ..)
 DEB_VERSION=${FREENET_VERSION_RELEASED}+${GIT_DESCRIBED}
 
-rm -rf fred-${DEB_VERSION}
-rm -rf fred-${FREENET_BRANCH}-dist
+rm -rf freenet-daemon-${DEB_VERSION}
+rm -rf freenet-daemon-${FREENET_BRANCH}-dist
 rm -f *.changes *.deb *.dsc *.debian.tar.gz *.orig.tar.bz2
 
-mkdir fred-${DEB_VERSION}
-cp -R fred-${FREENET_BRANCH} contrib-${FREENET_BRANCH} fred-${DEB_VERSION}
-find fred-${DEB_VERSION}/fred-${FREENET_BRANCH} -name .git|xargs rm -rf
-find fred-${DEB_VERSION}/fred-${FREENET_BRANCH} -name .cvsignore|xargs rm -rf
-find fred-${DEB_VERSION}/fred-${FREENET_BRANCH} -name .gitignore|xargs rm -rf
-find fred-${DEB_VERSION}/contrib-${FREENET_BRANCH} -name .git|xargs rm -rf
-tar cfj fred_${DEB_VERSION}.orig.tar.bz2 fred-${DEB_VERSION}
+mkdir freenet-daemon-${DEB_VERSION}
+cp -R fred-${FREENET_BRANCH} contrib-${FREENET_BRANCH} freenet-daemon-${DEB_VERSION}
+find freenet-daemon-${DEB_VERSION}/fred-${FREENET_BRANCH} -name .git|xargs rm -rf
+find freenet-daemon-${DEB_VERSION}/fred-${FREENET_BRANCH} -name .cvsignore|xargs rm -rf
+find freenet-daemon-${DEB_VERSION}/fred-${FREENET_BRANCH} -name .gitignore|xargs rm -rf
+find freenet-daemon-${DEB_VERSION}/contrib-${FREENET_BRANCH} -name .git|xargs rm -rf
+tar cfj freenet-daemon_${DEB_VERSION}.orig.tar.bz2 freenet-daemon-${DEB_VERSION}
 
 rm -f debian/seednodes.fref
 wget -O debian/seednodes.fref http://downloads.freenetproject.org/alpha/opennet/seednodes.fref
 
-cp -R debian fred-${DEB_VERSION}/debian
-find fred-${DEB_VERSION}/debian -name copyright | xargs sed -i 's/@RELEASE@/'${FREENET_BRANCH}'/g'
-find fred-${DEB_VERSION}/debian -name fred.docs | xargs sed -i 's/@RELEASE@/'${FREENET_BRANCH}'/g'
-find fred-${DEB_VERSION}/debian -name rules | xargs sed -i 's/@RELEASE@/'${FREENET_BRANCH}'/g'
-find fred-${DEB_VERSION}/debian -name rules | xargs sed -i 's/@REVISION@/'${GIT_DESCRIBED}'/g'
+cp -R debian freenet-daemon-${DEB_VERSION}/debian
+find freenet-daemon-${DEB_VERSION}/debian -name copyright | xargs sed -i 's/@RELEASE@/'${FREENET_BRANCH}'/g'
+find freenet-daemon-${DEB_VERSION}/debian -name freenet-daemon.docs | xargs sed -i 's/@RELEASE@/'${FREENET_BRANCH}'/g'
+find freenet-daemon-${DEB_VERSION}/debian -name rules | xargs sed -i 's/@RELEASE@/'${FREENET_BRANCH}'/g'
+find freenet-daemon-${DEB_VERSION}/debian -name rules | xargs sed -i 's/@REVISION@/'${GIT_DESCRIBED}'/g'
 
-cd fred-${DEB_VERSION} && dch -v ${DEB_VERSION}-${DEB_REVISION} "GIT SNAPSHOT RELEASE! TEST PURPOSE ONLY!" && dpkg-buildpackage -rfakeroot && cd ..
+cd freenet-daemon-${DEB_VERSION} && dch -v ${DEB_VERSION}-${DEB_REVISION} "GIT SNAPSHOT RELEASE! TEST PURPOSE ONLY!" && dpkg-buildpackage -rfakeroot && cd ..
 
-mkdir fred-${FREENET_BRANCH}-dist
-mv *.changes *.deb *.dsc *.debian.tar.gz *.orig.tar.bz2 fred-${FREENET_BRANCH}-dist
+mkdir freenet-daemon-${FREENET_BRANCH}-dist
+mv *.changes *.deb *.dsc *.debian.tar.gz *.orig.tar.bz2 freenet-daemon-${FREENET_BRANCH}-dist
 
 rm -f debian/seednodes.fref
-tar cfz debian.fred.tmpl.tar.gz debian
-cp debian.fred.tmpl.tar.gz fred-${FREENET_BRANCH}-dist
+tar cfz debian.freenet-daemon.tmpl.tar.gz debian
+cp debian.freenet-daemon.tmpl.tar.gz freenet-daemon-${FREENET_BRANCH}-dist
 
-cd fred-${FREENET_BRANCH}-dist
+cd freenet-daemon-${FREENET_BRANCH}-dist
 dpkg-scanpackages . /dev/null > Packages
 gzip -9 Packages
 dpkg-scansources . /dev/null > Sources
