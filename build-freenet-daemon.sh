f67e7d916c11575738d8ca48f690cd904fd788f0
Ximin Luo <infinity0@gmx.com>
2011-04-05 10:25:18 +0100

    simplfy the build process by building from submodule trees, rather than copying a repo - also download and verify seednodes.fref.gpg rather than the unencrypted/signed version

--- build-freenet-daemon.sh
+++ build-freenet-daemon.sh
@@ -2,8 +2,9 @@
 
 PACKAGE=freenet-daemon
 FREENET_VERSION_RELEASED=0.7.5
+DEV_BUILD=true
 
-USAGE="Usage: $0 [-c|-u|-o|-S|-h] [--] [BRANCH]"
+USAGE="Usage: $0 [-c|-u|-o|-S|-h] [--]"
 BOPT_CLEAN_ONLY=false
 BOPT_UPDATE=false
 BOPT_ORIG_ONLY=false
@@ -19,8 +20,7 @@ while getopts cuoSh o; do
 		cat <<-EOF
 		$USAGE
 
-		Build Freenet from fred-BRANCH and contrib-BRANCH into the freenet-daemon
-		debian package.
+		Build fred and contrib into the freenet-daemon debian package.
 
 		  -h            This help text.
 		  -c            Clean previous build products only.
@@ -41,78 +41,69 @@ log() {
 	echo "$PREFIX$@"
 }
 
-FREENET_BRANCH="$1"
-if [ -z "$FREENET_BRANCH" ]; then FREENET_BRANCH=official; fi
-REPO_FRED=${PACKAGE}/fred-${FREENET_BRANCH}
-REPO_EXT=${PACKAGE}/contrib-${FREENET_BRANCH}
-if ! [ -d ${REPO_FRED} ]; then echo "not a directory: ${REPO_FRED}"; echo "$USAGE"; exit 1; fi
-if ! [ -d ${REPO_EXT} ]; then echo "not a directory: ${REPO_EXT}"; echo "$USAGE"; exit 1; fi
+REPO_FRED=${PACKAGE}/fred
+REPO_EXT=${PACKAGE}/contrib
 
-GIT_DESCRIBED="$(cd ${REPO_FRED} && git describe --always --abbrev=4 && cd ..)"
-GIT_DESCRIBED_EXT="$(cd ${REPO_EXT} && git describe --always --abbrev=4 && cd ..)"
+GIT_DESCRIBED="$(cd ${REPO_FRED} && git describe --always --abbrev=4)"
+GIT_DESCRIBED_EXT="$(cd ${REPO_EXT} && git describe --always --abbrev=4)"
 DEB_VERSION=${FREENET_VERSION_RELEASED}+${GIT_DESCRIBED}
-DEB_REVISION="$(cd ${PACKAGE} && dpkg-parsechangelog | grep Version | cut -d- -f2)"
+DEB_REVISION="$(cd ${PACKAGE} && dpkg-parsechangelog | grep Version | grep -o '\-[^-]*$' | tail -c+2)"
 
-BUILD_DIR="freenet-daemon-${DEB_VERSION}"
-DIST_DIR="freenet-daemon-${FREENET_BRANCH}-dist"
+DIST_DIR="freenet-daemon-dist"
 
-log 0 "building freenet-daemon in $BUILD_DIR/"
-log 0 "packages will be saved to $DIST_DIR/"
+log 0 "building freenet-daemon; packages will be saved to $DIST_DIR/"
 
 #PS4="\[\033[01;34m\]\w\[\033[00m\]\$ "
 #set -x
 
 log 1 "cleaning previous build products..."
-rm -rf "$BUILD_DIR"
 rm -rf "$DIST_DIR"
 rm -f *.orig.tar.bz2 *.tmpl.tar.gz
 rm -f *.changes *.deb *.dsc *.debian.tar.gz
 if $BOPT_CLEAN_ONLY; then exit; fi
-mkdir "$BUILD_DIR" || exit 1
 mkdir "$DIST_DIR" || exit 1
 
 if $BOPT_UPDATE; then
 	log 1 "updating repos..."
-	cd ${REPO_FRED} && git pull origin && cd ..
-	cd ${REPO_EXT} && git pull origin && cd ..
+	cd ${REPO_FRED} && git pull origin && cd -
+	cd ${REPO_EXT} && git pull origin && cd -
+	# update seednodes
+	cd ${PACKAGE}/debian
+	# FIXME: upstream bug prevents this from working for the moment
+	#wget -N https://downloads.freenetproject.org/alpha/opennet/seednodes.fref.gpg || exit 1
+	wget -N http://downloads.freenetproject.org/alpha/opennet/seednodes.fref.gpg || exit 1
+	gpg --output seednodes.fref --decrypt seednodes.fref.gpg || exit 1
+	rm -f seednodes.fref.gpg
+	cd -
 fi
 
-log 1 "copying and editing source files..."
-log 1 "- note that this step will be *much* quicker if you \`git clean\` and \`ant clean-all\` beforehand"
-cp -aH ${REPO_FRED} ${REPO_EXT} "$BUILD_DIR" || exit 1
-cd "$BUILD_DIR"
-# remove cruft
-for path in fred-${FREENET_BRANCH} contrib-${FREENET_BRANCH}; do
-	cd "$path"
-	git clean -qfdx
-	find . -name .git -o -name .gitignore -o -name .cvsignore | xargs rm -rf
-	cd ..
+log 1 "updating submodules..."
+git submodule update || exit 1
+
+log 1 "cleaning source repos..."
+for path in ${REPO_FRED} ${REPO_EXT}; do
+	cd "$path" && git reset --hard HEAD && git clean -fdx && cd - || exit 1
 done
-cd ..
 
 log 1 "making original source archives..."
-tar cfj freenet-daemon_${DEB_VERSION}.orig.tar.bz2 "$BUILD_DIR" || exit 1
+tar --exclude-vcs -cjf freenet-daemon_${DEB_VERSION}.orig.tar.bz2 ${PACKAGE} || exit 1
 cp freenet-daemon_${DEB_VERSION}.orig.tar.bz2 "$DIST_DIR" || exit 1
-tar cfz debian.freenet-daemon.tmpl.tar.gz ${PACKAGE}/debian || exit 1
+tar --exclude-vcs -czf debian.freenet-daemon.tmpl.tar.gz ${PACKAGE}/debian || exit 1
 cp debian.freenet-daemon.tmpl.tar.gz "$DIST_DIR" || exit 1
-
-log 1 "copying and editing debian packaging files..."
-cp -a ${PACKAGE}/debian "$BUILD_DIR"/debian || exit 1
-cd "$BUILD_DIR"/debian
-# update seednodes
-rm -rf seednodes.fref && wget -q -O seednodes.fref http://downloads.freenetproject.org/alpha/opennet/seednodes.fref || exit 1
-# substitute variables
-ls -1 copyright freenet-daemon.docs rules | xargs sed -i \
-	-e 's/@REVISION@/'${GIT_DESCRIBED}'/g' \
-	-e 's/@EXTREVISION@/'${GIT_DESCRIBED_EXT}'/g' \
-	-e 's/@RELEASE@/'${FREENET_BRANCH}'/g' || exit 1
-cd ../..
 
 if $BOPT_ORIG_ONLY; then exit; fi
 
 log 1 "building debian packages..."
-cd "$BUILD_DIR"
-dch -v ${DEB_VERSION}-${DEB_REVISION} "GIT SNAPSHOT RELEASE! TEST PURPOSE ONLY!" && dpkg-buildpackage -rfakeroot $BOPT_DPKG
+cd ${PACKAGE}
+if $DEV_BUILD; then
+	CHLOG=debian/changelog
+	cp "$CHLOG" "$CHLOG.old" || exit 1
+	dch -v ${DEB_VERSION}-${DEB_REVISION} "GIT SNAPSHOT RELEASE! TEST PURPOSE ONLY!" || exit 1
+	undch() { if [ -f "$CHLOG.old" ]; then mv "$CHLOG.old" "$CHLOG"; fi }
+	trap undch EXIT INT TERM KILL
+	echo "\033[36;1m$CHLOG\033[m has been modified; \033[31;1mplease do NOT commit it to source control.\033[m It will be reverted when this command exits or is aborted."
+fi
+dpkg-buildpackage -rfakeroot $BOPT_DPKG || exit 1
 cd ..
 
 log 1 "saving built packages..."
